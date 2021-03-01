a37f1f1704520ce3e28a1fa971c25b333797980c
SalahAmer2 <salahayman2@gmail.com>
2021-03-01 09:28:41 +0200

    added list star

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -39,6 +39,49 @@ function Home() {
 
     const [dateValue, onChange] = useState(new Date());
 
+    const StarIconUnselected = () => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="#FFFFFF"
+            stroke="#CAA53D"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+        </svg>
+    )
+
+    const StarIconSelected = () => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="#CAA53D"
+            stroke="#CAA53D"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+        </svg>
+    )
+
+    const star = state.starredOrNot ? <StarIconSelected /> : <StarIconUnselected />;
+
+    const handleStar = () => {
+        setState(prevState => {
+            return {
+                ...prevState,
+                starredOrNot: !state.starredOrNot
+            };
+        });
+    }
+
     return (
         <div className="home">
             {/* ToDo list adder */}
@@ -82,6 +125,12 @@ function Home() {
                     <DateTimePicker
                         onChange={onChange}
                     />
+
+                    <div
+                        onClick={handleStar}
+                    >
+                        {star}
+                    </div>
 
                     <input type="submit" value="Submit" />
                 </form>
