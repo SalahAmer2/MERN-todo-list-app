57b2001ae7e65e6a9e426aa7db300806a704257f
SalahAmer2 <salahayman2@gmail.com>
2021-03-03 10:38:17 +0200

    Prepared for deployment

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('MONGODB_URI');
+
+const connectDB = async () => {
+    try {
+
+        await mongoose.connect(db || 'mongodb://localhost/mern_todo', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected!');
+
+    } catch (err) {
+        console.error(err.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-// const cors = require('cors');
 const path = require('path');
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+connectDB();
 
 const routes = require('./routes/api')
 
@@ -16,10 +18,10 @@ const routes = require('./routes/api')
 //     useUnifiedTopology: true
 // });
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_todo', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_todo', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
 
 mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected')
@@ -35,8 +37,14 @@ app.use(morgan('tiny'));
 
 app.use('/api', routes)
 
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+    //Set static folder
     app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 }
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
